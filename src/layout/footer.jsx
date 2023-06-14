b659d360349a55fef4d472ba7eede814585ed02c
Lusindr <113450709+Lusindr@users.noreply.github.com>
2023-06-14 09:52:03 +0300

    first commit

---
+++ src/layout/footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return <footer className="page-footer depp-purple accent-1">
+        <div className="footer-copyright">
+            <div className="container">
+                {new Date().getFullYear()}
+                <a className="grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+        </div>
+    </footer>
+}
+export { Footer };
